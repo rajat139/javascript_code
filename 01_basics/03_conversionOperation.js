39510427f75b4ab14dc86c2cda6a08d1ff536bf1
rajat139 <rajatsaxena139@gmail.com>
2025-01-02 05:57:07 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = 33
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NAN
+// true => 1;, false=> 0
+
+let isLoggedIn = "" 
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false; 
+// "Sita-Ram" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
