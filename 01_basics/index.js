c834adf4080e3878fb75659271627abc9527b2d5
rajat139 <rajatsaxena139@gmail.com>
2025-05-13 05:04:26 +0000

    Revision

--- 01_basics/index.js
+++ 01_basics/index.js
@@ -1,1 +1,16 @@
 console.log("Hello! Everyone")
+
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2    // not allowed 
+
+console.log(accountId);
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
