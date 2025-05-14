6112db745005099d13c7409e3cff43db9f19713f
rajat139 <rajatsaxena139@gmail.com>
2025-05-14 08:34:35 +0000

    revision datatypes

--- 01_basics/index.js
+++ 01_basics/index.js
@@ -1,4 +1,4 @@
-console.log("Hello! Everyone")
+// console.log("Hello! Everyone")
 
 const accountId = 144553
 let accountEmail = "hitesh@google.com"
@@ -8,7 +8,7 @@ let accountState;
 
 // accountId = 2    // not allowed 
 
-console.log(accountId);
+// console.log(accountId);
 
 /*
 Prefer not to use var
@@ -19,20 +19,40 @@ accountEmail = "hc@hc.com"
 accountPassword = "21212121"
 accountCity = "Bengaluru"
 
-console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+    // table
+// console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
 
 
-    // datatypes
+    // ******** datatypes   *********
 
 "use strict";   // treat all js code as newer version
 // alert(3 + 3)    // we are using nodejs not browser
 
-console.log(3
-    +3) // code readabiliity should be high
+// console.log(3
+    // +3) // code readabiliity should be high
 
-console.log("Hello")
+// console.log("Hello")
 
 let name = "Abhinabh"
 let age = 18
 let isLoggedIn = true
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+// console.log(typeof "hitesh");
+// console.log(typeof age);
+// console.log(typeof undefined);
+// console.log(typeof null);
+
+
+
+
 
