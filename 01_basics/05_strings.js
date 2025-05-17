8ea21aedf983f2a6efbf2418fb571fa486025806
rajat139 <rajatsaxena139@gmail.com>
2025-05-17 04:49:11 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,37 @@
+const name = "Abhinav"
+const repoCount = 50
+// console.log(name + repoCount + " value");
+
+// console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`);
+
+const gameName = new String('Abhinav')
+// console.log(gameName[0])
+// console.log(gameName.__proto__)
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('n'))
+
+const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+const anotherGame = gameName.slice(-8, 4)
+// console.log(anotherGame)
+
+const newStringOne = "   Abhinav      "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://Abhinav.com/Abhinav%20chaudhary"
+// console.log(url.replace('%20', '-'));
+// console.log(url.includes('Abhi'))
+// console.log(url.split('/'))
+// console.log(url.valueOf())
+
+
+
+
+
+
+
