c4c2932142b38a039f0ed374048c8da45f19de76
rajat139 <rajatsaxena139@gmail.com>
2025-05-17 15:11:47 +0000

    array

---
+++ 02_basics/arrays.js
@@ -0,0 +1,33 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeroes = ["shaktiman", "Naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4, 5)
+
+// console.log(myHeroes[1]);
+
+// arrays methods
+
+// myArr.push(6)   // add a new element
+// myArr.push(7)
+// myArr.pop()     // remove last element in array
+
+// myArr.unshift(9)    // add element in start in Array
+// myArr.shift()       // remove first element in array
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(4));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr);
+
+
+
+
+
+
+
+
