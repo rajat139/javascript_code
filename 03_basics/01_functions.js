dff2dd347ae79054fc9da9daf626af88f556c866
rajat139 <rajatsaxena139@gmail.com>
2025-05-20 09:54:02 +0000

    function part2

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -32,7 +32,39 @@ function loginUserMessage(username = "sam"){
     return `${username} just logged in`
 }
 // console.log(loginUserMessage("abhinav"));
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
+
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+// console.log(calculateCartPrice(100, 200, 300, 2000));
+
+
+const user = {
+    userName : "Abhinav",
+    price : 199
+}
+
+function handleObject(anyObject){
+    console.log(`username is ${anyObject.userName} and price ${anyObject.price}`);
+}
+// handleObject(user)
+handleObject({
+    userName : "sam",
+    price : 399
+})
+
+const mynewArr = [200, 300, 500, 600]
+
+function returnsecondvalue(getArr){
+    return getArr[1]
+}
+// console.log(returnsecondvalue(mynewArr));
+console.log(returnsecondvalue([200, 300, 500, 600]));
+
+
+
 
 
 
