4d731bb5c1a00cb2c512e040cef124d5f80a96b9
rajat139 <rajatsaxena139@gmail.com>
2025-05-20 10:26:05 +0000

    Scope level and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,14 +3,45 @@ let a = 300
 if (true){
     let a = 10
     const b = 20
-    console.log("Inner : ", a);
+    // console.log("Inner : ", a);
 }
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
 
 
+function one(){
+    const username = "abhinav"
+
+    function two(){
+        const website = "youtube"
+        // console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+one()
+
+if(true){
+    const username = "abhinav"
+    if(username === "abhinav"){
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
 
 
+// ++++++++++++++++++ interesting ++++++++++++++++++++++
 
+// console.log(addone(5));     // 6
+function addone(num){
+    return num + 1
+}
 
+// addtwo(5)       // Cannot access 'addtwo' before initialization
+const addtwo = function(num){
+    return num + 2
+}
+// console.log(addtwo(5));     // 7
