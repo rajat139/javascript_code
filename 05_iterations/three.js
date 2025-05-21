910add5e0a8e1dd010fc955690f28586db0ee7bd
rajat139 <rajatsaxena139@gmail.com>
2025-05-21 13:34:24 +0000

    high order array loops

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -21,6 +21,7 @@ const map = new Map()
 map.set('IN', "India")
 map.set('USA', "United States of America")
 map.set('Fr', "France")
+map.set('IN', "India")
 // console.log(map)
 for (const [key, value] of map) {
     // console.log(key, ':-', value)
